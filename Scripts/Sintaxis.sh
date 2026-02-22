67efa081af4e2b159a6c38840ea1c7e9503398aa
jose <jose@practica.com>
2026-02-22 21:19:14 +0100

    Actualización automática

--- Scripts/Sintaxis.sh
+++ Scripts/Sintaxis.sh
@@ -12,3 +12,30 @@
 # "$usuario" = "$nombre"
 
 #calular resto=$(expr $num % 2)
+
+#if else
+if [$a -lt $b]
+then
+    echo "$a ES MAYOR"
+else
+    echo "$b ES MAYOR"
+fi
+
+
+#switch uso
+case "$variable" in
+    valor1)
+        # comandos
+        ;;
+    valor2)
+        # comandos
+        ;;
+    *)
+        # caso por defecto
+        ;;
+esac
+
+#bucle while
+while [ condición ]; do
+    # comandos a ejecutar
+done
