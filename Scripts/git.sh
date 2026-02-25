cc4f93056df962c1bd68080d358d1373a2027252
Revan115x <217095250+Revan115x@users.noreply.github.com>
2026-02-25 09:53:16 +0100

    Actualización automática

--- Scripts/git.sh
+++ Scripts/git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Script para actualizar GitHub sin escribir las lineas por pereza
-cd /home/inf1/Desktop/Programacion/ScriptBash/RepositorioGit
+cd /home/inf1/Desktop/Programacion/ScriptBash
 git add .
 git commit -m "Actualización automática"
 git push origin main
+
